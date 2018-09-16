d64d7edca42259115389ed36ed935ea17236354e
BoBeenLee <cultist_tp@naver.com>
2018-09-16 17:50:39 +0900

    로고 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,10 +4,7 @@ import React, { Component } from 'react';
 class Logo extends Component {
     public render() {
         return (
-            <div>
-                <img src={Image.ic_appicon_primary} />
-                Hello World
-            </div>
+            <img src={Image.ic_appicon_primary} />
         );
     }
 }
