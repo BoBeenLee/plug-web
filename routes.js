aa1271ea760d33c459053a0dfb3b3b48af27d94c
Jonghee Woo <jongheepaper@gmail.com>
2018-10-21 14:29:06 +0900

    소개 페이지 추가

--- routes.js
+++ routes.js
@@ -4,6 +4,11 @@ const routers = new Routes().add({
   name: "Hello",
   page: "hello",
   pattern: "/hello"
+},
+{
+  name: "About",
+  page: "About",
+  pattern: "/about"
 });
 
 module.exports = routers;
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            aboutPage
+        </div>
+    );
+};
+
+export default AboutPage;
