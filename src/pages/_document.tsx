33b61d503c743b77fcdd9cc89a8faca2a306d33b
BoBeenLee <cultist_tp@naver.com>
2019-03-08 19:40:02 +0900

    ie 코드 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { ServerStyleSheet } from "styled-components";
 import { injectGlobal } from "styled-components";
 
-import { isIE } from '../utils/navigator';
-
 // tslint:disable-next-line:no-unused-expression
 injectGlobal`
   @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
@@ -110,7 +108,48 @@ export default class MyDocument extends Document<any> {
           {this.props.styleTags}
           <script dangerouslySetInnerHTML={{
             __html: `
-            ${isIE() ? `window.location.href = "./static/guide.html";` : ``}
+            if (detectIE()) {
+              window.location.href = "./static/guide.html";
+            }
+            function detectIE() {
+              var ua = window.navigator.userAgent;
+            
+              // Test values; Uncomment to check result …
+            
+              // IE 10
+              // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+            
+              // IE 11
+              // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+            
+              // Edge 12 (Spartan)
+              // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+            
+              // Edge 13
+              // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+            
+              var msie = ua.indexOf('MSIE ');
+              if (msie > 0) {
+                // IE 10 or older => return version number
+                return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+              }
+            
+              var trident = ua.indexOf('Trident/');
+              if (trident > 0) {
+                // IE 11 => return version number
+                var rv = ua.indexOf('rv:');
+                return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+              }
+            
+              var edge = ua.indexOf('Edge/');
+              if (edge > 0) {
+                // Edge (IE 12+) => return version number
+                return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+              }
+            
+              // other browser
+              return false;
+            }
           `}} />
         </Head>
         <body>
